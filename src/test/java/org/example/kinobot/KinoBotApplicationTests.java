3b35d239c35f5aeccd78e31ae89835d1b6d8b0af
Muslimbek Yo'ldoshov <muslimyoldoshov41@gmail.com>
2024-12-01 20:28:02 +0500

    git commit 'First commit'

---
+++ src/test/java/org/example/kinobot/KinoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
